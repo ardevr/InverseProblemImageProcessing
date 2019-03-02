6df2df5cb4a4fbff0776c3d63e5e91bc1cf44e88
Evrard Garcelon <evrard.garcelon@gmail.com>
2019-03-02 16:04:19 +0100

    Experience and modified env

--- environnement.py
+++ environnement.py
@@ -7,50 +7,54 @@ Created on Thu Feb 21 16:12:47 2019
 """
 
 import numpy as np
-from scipy.special import j0,y0
-from numpy.fft import fft,ifft
+from scipy.special import j0, y0
+from numpy.fft import fft, ifft
 from gaussian_process import GaussianProcess
 
+
 class Environnement():
-    
-    def __init__(self, c0 = 1, L = 50, N = 10, nb_timesteps = 100) :
-        
+
+    def __init__(self, c0=1, L=50, N=10, nb_timesteps=100, seed = 1, eps = 10**-10):
+
         self.L = L
+        self.eps = eps
         self.N = N
         self.c0 = c0
-        self.G_hat = lambda w,x,y : 1j/4*(np.nan_to_num(j0(w*np.linalg.norm(x-y))) + 1j*np.nan_to_num(y0(w*np.linalg.norm(x-y))))
-        self.G = lambda t,x,y : 0 # Trouver valeur G
+        self.G_hat = lambda w, x, y: 1j / 4 * \
+            (np.nan_to_num(j0(w * np.linalg.norm(x - y))) + 1j * np.nan_to_num(
+                    y0(w * np.linalg.norm(x - y))))
+        self.G = lambda t, x, y:  np.nan_to_num(1/(2*np.pi)*(t**2 > np.dot(x-y,x-y) + self.eps)/np.sqrt(t**2 - np.dot(x-y,x-y)))
         self.nb_timesteps = nb_timesteps
+        self.seed = seed
+
+    def compute_signal(self, x1, x2, tau, T, eps=10**-10):
         
-        
-    def compute_signal(self,x1, x2, tau, T,eps = 10**-10) :
-        time_discretization = np.linspace(eps,T,self.nb_timesteps)
-        thetas = np.random.uniform(size = self.N)
-        y = np.zeros((self.N,2))
-        y[:,0] = np.cos(2*np.pi*thetas)
-        y[:,1] = np.sin(2*np.pi*thetas)
-        cov = lambda x : np.exp(-x**2/4)*(2 - x**2)/(4*np.sqrt(2)) 
+        time_discretization = np.linspace(eps, T, self.nb_timesteps)
+        thetas = np.random.uniform(size=self.N)
+        y = np.zeros((self.N, 2))
+        y[:, 0] = self.L*np.cos(2 * np.pi * thetas)
+        y[:, 1] = self.L*np.sin(2 * np.pi * thetas)
+        self.y = y
+        def cov(x): return np.exp(-x**2 / 4) * (2 - x**2) / (4 * np.sqrt(2))
         gp = GaussianProcess(cov)
         signal_x1 = 0
         signal_x2 = 0
-        for i in range(self.N) :
-            temp_gp = gp.generate_n_var(self.nb_timesteps, time_discretization, fourier = True)
-            temp_G1 = fft(self.G_hat(time_discretization,x1,y[i]))
-            temp_G2 = fft(self.G_hat(time_discretization + tau,x2,y[i]))
-            signal_x1 += np.real(ifft(temp_G1*temp_gp))
-            signal_x2 += np.real(ifft(temp_G2*temp_gp))
-        return self.L/np.sqrt(self.N)*signal_x1,self.L/np.sqrt(self.N)*signal_x2
+        for i in range(self.N):
+            temp_gp = gp.generate_n_var(
+                self.nb_timesteps, time_discretization, fourier=False)
+            temp_G1 = fft(self.G(time_discretization, x1, y[i]))
+            temp_G2 = fft(self.G(time_discretization + tau, x2, y[i]))
+            signal_x1 += np.real(ifft(temp_G1 * temp_gp))
+            signal_x2 += np.real(ifft(temp_G2 * temp_gp))
+        return self.L / np.sqrt(self.N) * \
+            signal_x1, self.L / np.sqrt(self.N) * signal_x2, time_discretization
+
+if __name__ == '__main__' : 
+    env = Environnement(N = 50)
+    x1 = x2 = np.ones(2)
+    tau = 1
+    u,u_lagged, time  = env.compute_signal(x1,x2,tau, 100, eps = 0)
+    import pylab as plt
+    plt.scatter(time, u,color = 'red',label = 'u')
+    plt.scatter(time, u_lagged, color = 'blue',label = 'u_lagged')
     
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
--- gaussian_process.py
+++ gaussian_process.py
@@ -11,20 +11,26 @@ from numpy.fft import fft,ifft
 
 class GaussianProcess():
     
-    def __init__(self,cov = None) :
+    def __init__(self,cov = None, seed = 1) :
         self.cov = cov
+        self.seed = seed
         
-    def generate_n_var(self,n,points, fourier = False) :
+    def generate_n_var(self,n,points, fourier = False, eps = 10**-5) :
         
         assert isinstance(points,np.ndarray)
         assert n == len(points)
 
         cov_vector = np.zeros(n)
         cov_vector = self.cov(points)
+        np.random.seed = self.seed
         y = np.random.randn(n)
         z_fourier = np.sqrt(fft(cov_vector))*fft(y)
         if fourier :
             return z_fourier
         else : 
-            return np.real(ifft(z_fourier))
+            temp_vec = ifft(z_fourier)
+            if np.sum(np.abs(np.imag(temp_vec))) < eps :
+                return np.real(ifft(z_fourier))
+            else : 
+                raise Exception('Inverse Fourier Transform not real')
     
