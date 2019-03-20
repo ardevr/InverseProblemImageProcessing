dcf625d45e6ae41f03dc0095893c029dd3e46fff
Evrard Garcelon <evrard.garcelon@gmail.com>
2019-03-20 06:20:10 +0100

    corrected

--- Experience.py
+++ Experience.py
@@ -11,14 +11,15 @@ import numpy as np
 from environnement import Environnement
 from scipy.integrate import quad
 import pylab as plt
-from tqdm import tqdm
-from scipy.special import j0, y0
-from scipy.stats import randint
+# from tqdm import tqdm
+from scipy.special import j0, y0, hankel1
+from tqdm import trange
+# from scipy.stats import randint
 from numpy.fft import fft, ifft
-from scipy.signal import fftconvolve
+# from scipy.signal import fftconvolve
+# from numpy import correlate
 
-
-import warnings
+# import warnings
 
 
 def emp_cross_corr(u_1, u_2, times, verbose=True) :
@@ -30,14 +31,9 @@ def emp_cross_corr(u_1, u_2, times, verbose=True) :
     u2_tilde = np.zeros(2*M)
     u1_tilde[M:] = u1
     u2_tilde[M:] = u2
-    DFT_C = ((-1)**np.linspace(0,2*M-1,2*M,dtype = 'int'))*np.conjugate(fft(u1_tilde))*fft(u2_tilde)
-    # result = fftconvolve(u1, u2,mode = 'full')/times[-1]
+    DFT_C = np.conjugate(fft(u1_tilde))*fft(u2_tilde)*((-1)**np.linspace(0,2*M-1,2*M))
     C_TN = ifft(DFT_C)/M
-    # print(np.max(np.abs(np.imag(C_TN))))
     C_TN = np.real(C_TN)
-    # plt.figure(1)
-    # plt.plot(C_TN)
-    # plt.show()
     if verbose:
         print('Done')
     return C_TN
@@ -45,25 +41,26 @@ def emp_cross_corr(u_1, u_2, times, verbose=True) :
 def exp_emp_cross_cor(tau, x1, x2, N, env, verbose=True):
     if verbose:
         print('Computing exp_emp_cross_corr ...')
-    assert N == env.N
     ys = env.ys
     c0 = env.c0
     F_hat = lambda w : env.fourier_cov(w)
-    if not isinstance(ys, np.ndarray):
-        ys = np.array(ys)
     temp_cor = 0
-    for y in  ys :
+    i = 0
+    while i < (len(ys)) :
+        y = ys[i]
         norme_1 = np.linalg.norm(x1 - y)/c0
         norme_2 = np.linalg.norm(x2 - y)/c0
-        a, b = 0, +np.inf
-        inte_1,_ = quad(lambda w : F_hat(w)*y0(w*norme_1)*y0(w*norme_2)*np.cos(w*tau),a,b, limit = 100)
-        inte_2,_ = quad(lambda w : F_hat(w)*y0(w*norme_2)*j0(w*norme_1)*np.sin(w*tau),a,b, limit = 100)
-        inte_3,_ = quad(lambda w : -F_hat(w)*y0(w*norme_1)*j0(w*norme_2)*np.sin(w*tau),a,b, limit = 100)
-        inte_4,_ = quad(lambda w : F_hat(w)*j0(w*norme_1)*j0(w*norme_2)*np.cos(w*tau),a,b, limit = 100)
-        temp_cor += 1/8*(inte_1 + inte_2 + inte_3 + inte_4)
+        a, b = -np.inf, +np.inf
+        inte_1,_ = quad(lambda w : F_hat(w)*np.imag(hankel1(0,w*norme_1))*np.imag(hankel1(0,w*norme_2))*np.cos(w*tau),a,b, limit = 100)
+        inte_2,_ = quad(lambda w : F_hat(w)*np.imag(hankel1(0,w*norme_2))*np.real(hankel1(0,w*norme_1))*np.sin(w*tau),a,b, limit = 100)
+        inte_3,_ = quad(lambda w : -F_hat(w)*np.imag(hankel1(0,w*norme_1))*np.real(hankel1(0,w*norme_2))*np.sin(w*tau),a,b, limit = 100)
+        inte_4,_ = quad(lambda w : F_hat(w)*np.real(hankel1(0,w*norme_1))*np.real(hankel1(0,w*norme_2))*np.cos(w*tau),a,b, limit = 100)
+        temp_result =  1/16*(inte_1 + inte_2 + inte_3 + inte_4)
+        temp_cor += temp_result
+        i = i+1
     if verbose:
         print('Done')
-    return temp_cor/(np.pi**2*N)
+    return temp_cor/(2*np.pi*N)
 
 def cross_1(tau, x1, x2, L, env, verbose=True):
 
@@ -76,32 +73,47 @@ def cross_1(tau, x1, x2, L, env, verbose=True):
         norme_1 = np.linalg.norm(x1 - y)/c0
         norme_2 = np.linalg.norm(x2 - y)/c0
         a, b = 0, +np.inf
-        inte_1,_ = quad(lambda w : F_hat(w)*y0(w*norme_1)*y0(w*norme_2)*np.cos(w*tau),a,b, limit = 100)
-        inte_2,_ = quad(lambda w : F_hat(w)*y0(w*norme_2)*j0(w*norme_1)*np.sin(w*tau),a,b, limit = 100)
-        inte_3,_ = quad(lambda w : -F_hat(w)*y0(w*norme_1)*j0(w*norme_2)*np.sin(w*tau),a,b, limit = 100)
-        inte_4,_ = quad(lambda w : F_hat(w)*j0(w*norme_1)*j0(w*norme_2)*np.cos(w*tau),a,b, limit = 100)
+        inte_1,_ = quad(lambda w : F_hat(w)*np.imag(hankel1(0,w*norme_1))*np.imag(hankel1(0,w*norme_2))*np.cos(w*tau),a,b, limit = 100)
+        inte_2,_ = quad(lambda w : F_hat(w)*np.imag(hankel1(0,w*norme_2))*np.real(hankel1(0,w*norme_1))*np.sin(w*tau),a,b, limit = 100)
+        inte_3,_ = quad(lambda w : -F_hat(w)*np.imag(hankel1(0,w*norme_1))*np.real(hankel1(0,w*norme_2))*np.sin(w*tau),a,b, limit = 100)
+        inte_4,_ = quad(lambda w : F_hat(w)*np.real(hankel1(0,w*norme_1))*np.real(hankel1(0,w*norme_2))*np.cos(w*tau),a,b, limit = 100)
         return  1/8*(inte_1 + inte_2 + inte_3 + inte_4)
+ #       f = lambda w : np.real(F_hat(w)*np.conjugate(1j/4*hankel1(0,w*norme_1))
+ #       *1j/4*hankel1(0,w*norme_2)*np.exp(-1j*tau*w))
+ #       temp_res, err = quad(f,a,b)
+ #      return temp_res
+    
     b = 2 * np.pi
     a = 0
     result,_ = quad(f, a, b)
     if verbose:
         print('Done')
-    return result / (8 * np.pi**3)
+    return result / (2 * np.pi**2)
 
-def C_asy(tau, x1, x2,  env, verbose=True):
+def C_asy(tau, x1, x2,  env, verbose=True, eps = 10**-10):
     if verbose:
         print('Computing C_asy...')
 
     norme = np.linalg.norm(x1-x2)/env.c0
-    def f(w): return env.fourier_cov(w) * j0(w*norme) * np.cos(w*tau) / w
-
-    result = env.c0*quad(f, a=0, b=+np.inf)[0]/(4*np.pi**3)
-
+    
+    if norme > eps :
+        def f(w): return env.fourier_cov(w) * np.real(hankel1(0,w*norme)) * np.cos(w*tau) / w
+        result = env.c0*quad(f, a=-np.inf, b=+np.inf)[0]
+    else :
+        F_hat = lambda w : env.fourier_cov(w)
+        def f(theta):
+            y = L*np.array([np.cos(theta),np.sin(theta)])
+            a, b = 0, +np.inf
+            inte, _ = quad(lambda w : np.cos(w*tau)*F_hat(w)*np.linalg.norm(env.G_hat(w,x1,y))**2, a, b)
+            return  1/8*inte
+        a, b = 0, 2*np.pi
+        result, _ = quad(f, a, b)
+        result = result*np.pi*L
     if verbose:
         print('Done')
-    return result
-
-
+    return result/(4*np.pi**2)
+        
+    
 def plot_exp(env,X) :
     plt.figure(50)
     plt.scatter(X[:,0],X[:,1],marker = '^', color = 'red', label = 'x')
@@ -112,147 +124,165 @@ def plot_exp(env,X) :
 
 def estimated_c0(env,x1,x2,C_TN,tau):
     return((x2[0]-x1[0])/(abs(C_TN.argmax()-env.nb_timesteps)*tau))
+    
+def plot_c_asy(x1, x2, env, low_tau = 1, high_tau = 50, nb_steps = 100) :
+    taus = np.linspace(low_tau, high_tau, nb_steps)
+    result = np.zeros(len(taus))
+    for i in trange(len(taus), desc = 'Computing C_asy') :
+        tau = taus[i]
+        result[i] = C_asy(tau, x1, x2, env, verbose = False)
+    return result
 
+def plot_c_1(x1, x2, env, low_tau = 1, high_tau = 50, nb_steps = 100) :
+    taus = np.linspace(low_tau, high_tau, nb_steps)
+    result = np.zeros(len(taus))
+    for i in trange(len(taus), desc = 'Computing C_1') :
+        tau = taus[i]
+        result[i] = cross_1(tau, x1, x2, env.L, env, verbose=False)
+    return result
+
+def plot_exp_emp_cross(x1, x2, env, low_tau = 1, high_tau = 50, nb_steps = 100):
+    taus = np.linspace(low_tau, high_tau, nb_steps)
+    result = np.zeros(len(taus))
+    for i in trange(len(taus), desc = 'Computing exp_emp_cross') :
+        tau = taus[i]
+        result[i] = exp_emp_cross_cor(tau, x1, x2, env.N, env, verbose=False)
+    return result
 
 
 if __name__ == '__main__':
-
-    L, N, T = 50, 100, 10**3
+        
+    L, N, T = 50, 100, 100
     X = np.zeros((5,2))
     X[:,0] = -15 + 5*np.linspace(1,5,5,dtype = 'int')
     x1, x2, x3, x4, x5 = X[0], X[1], X[2], X[3], X[4]
     fourier_cov = lambda w : w**2*np.exp(-w**2)
-    import time
-    debut = time.clock()
-    env = Environnement(nb_timesteps = int(3*T), L = L, N = N, fourier_cov= fourier_cov)
-    Ninter = 2
-    C_TNinter = []
-    for i in range(Ninter) :
-        u1,u2,times,_ = env.compute_signal(x1, x2, T)
-        C_TNinter.append(emp_cross_corr(u1, u2, times, verbose=False))
-    C_TN = C_TNinter[0]
-    for i in range(1,len(C_TNinter)):
-        C_TN = C_TNinter[i] +C_TN
-    C_TN = C_TN/len(C_TNinter)
-    fin = time.clock()
-    tau = T/env.nb_timesteps
-    print('Temps emp_cross_corr : ', fin - debut)
-    # debut = time.clock()
-    # exp_emp_corr = exp_emp_cross_cor(tau, x1, x2, N, env)
-    # fin = time.clock()
-    # print('Temps exp emp corr : ', fin - debut)
-    # debut = time.clock()
-    # c_1 = cross_1(tau, x1, x2, L, env)
-    # fin = time.clock()
-    # print('Temps cross_1 : ', fin - debut)
-    # debut = time.clock()
-    # c_asy = C_asy(tau, x1, x2, env)
-    # fin = time.clock()
-    # print('Temps C_asy : ', fin - debut)
+    #%%
+    env = Environnement(nb_timesteps = 7000, L = L, N = N, fourier_cov= fourier_cov)
+    u1,u2,times,_ = env.compute_signal(x1, x2, T)
+    C_TN = emp_cross_corr(u1, u2, times, verbose=False)  
+    
+    nb_steps = 100
+    result_asy = plot_c_asy(x1,x3, env, low_tau = times[0], high_tau = times[-1], nb_steps= nb_steps)
+    result_1 = plot_c_1(x1,x3, env, low_tau = times[0], high_tau = times[-1], nb_steps= nb_steps)
+    result_2 = plot_exp_emp_cross(x1,x3, env, low_tau = times[0], high_tau = times[-1], nb_steps= nb_steps)
+    taus = np.linspace(times[0], times[-1],nb_steps)
+    plt.figure(1)
+    plt.plot(taus, 2*L*result_1, linewidth = 1.5, color = 'blue', label = 'C_1', marker = '+')
+    plt.plot(taus, 2*L*result_2, linewidth = 1.5, color = 'green', label = 'exp_emp_cross', marker = 'o')
+    plt.plot(taus, result_asy, linewidth = 1.5, color = 'red', label = 'C_asy', marker = '^')
+    plt.plot(times, 2*L*C_TN[len(u1):], linewidth = 1.5, color = 'magenta', label = 'Empirical Cross correlation')
+    plt.plot()
+    plt.legend()
+    plt.show()
+    
+    #%%
+    
+    nb_repetitions = 10
+    nb_timesteps = 5000
+    high_N, low_N = 150, 30
+    high_T, low_T = 500, 100
+    Ns, Ts = np.linspace(low_N, high_N, 20, dtype = 'int'), np.linspace(low_T, high_T, 20, dtype = 'int')
+    tau_max = np.linalg.norm(x1-x3)
+    random_index_N = np.random.choice(
+            np.linspace(0,len(Ns)-1,len(Ns),dtype = 'int'))
+    random_N = Ns[random_index_N]
 
-    # print('Erreur : ', np.min(np.abs(C_TN - c_1)/np.abs(c_1)))
-    # print('Argmin : ', np.argsort((np.abs(C_TN - c_1)/np.abs(c_1)))[:10])
-    # plt.plot(C_TN)
-    # plt.show()
-    # print(max(C_TN),c_asy,c_1,exp_emp_corr)
-
-    # Question 3
-    # x2 = x1
-    # u1,u2,times,_ = env.compute_signal(x1, x2, T)
-    # C_TN=emp_cross_corr(u1, u2, times, verbose=False)
-    # c_asy = C_asy(tau, x1, x2, env)
-    # c_1 = cross_1(tau, x1, x2, L, env)
-    # exp_emp_corr = exp_emp_cross_cor(tau, x1, x2, N, env)
-    # print(max(C_TN),c_asy,c_1,exp_emp_corr)
-
-    # Question 4
-    # c0= 3
-    # x2 = x5
-    # env = Environnement(nb_timesteps = int(3*T), L = L, N = N, fourier_cov= fourier_cov,c0=c0)
-    # Ninter = 7
-    # C_TNinter = []
-    # for i in range(Ninter) :
-    #     u1,u2,times,_ = env.compute_signal(x1, x2, T)
-    #     C_TNinter.append(emp_cross_corr(u1, u2, times, verbose=False))
-    # C_TN = C_TNinter[0]
-    # for i in range(1,len(C_TNinter)):
-    #     C_TN = C_TNinter[i] +C_TN
-    # C_TN = C_TN/len(C_TNinter)
-    # print(estimated_c0(env,x1,x2,C_TN,tau),c0)
-
+    C_TNs = np.zeros((nb_repetitions, len(Ts)))
+    
+    for i in range(nb_repetitions) :
+        for j in range(len(Ts)) :
+                N,T = 1*random_N, Ts[j]
+                delta_t = T/(nb_timesteps-1)
+                index_tau_max = int(tau_max/delta_t)
+                env = Environnement(nb_timesteps = nb_timesteps, L = L, N = N, 
+                                    fourier_cov= fourier_cov)
+                u1,u2,times,_ = env.compute_signal(x1, x3, T)
+                C_TN = emp_cross_corr(u1, u2, times, verbose=False) 
+                C_TN = C_TN[len(u1)+ index_tau_max]
+                C_TNs[i,j] = C_TN
+    mean_C = np.mean(C_TNs, axis = 0)
+    q = 0.1
+    q_bound = np.quantile(C_TNs, q, axis = 0)
+    Q_bound = np.quantile(C_TNs, 1-q, axis = 0)
+    plt.figure(2)
+    plt.fill_between(Ts, q_bound, Q_bound, alpha=0.15, linewidth=1.5, 
+                     color='red')
+    plt.plot(Ts, mean_C, linewidth = 1.5, color ='red', marker = '^')
+    
+    random_index_T = np.random.choice(
+    np.linspace(0,len(Ts)-1,len(Ts),dtype = 'int'))
+    random_T = Ts[random_index_T]
+    C_TNs = np.zeros((nb_repetitions, len(Ts)))
+    
+    for i in range(nb_repetitions) :
+        for j in range(len(Ns)) :
+                N,T = Ns[j], 1*random_T
+                delta_t = T/(nb_timesteps-1)
+                index_tau_max = int(tau_max/delta_t)
+                env = Environnement(nb_timesteps = nb_timesteps, L = L, N = N, 
+                                    fourier_cov= fourier_cov)
+                u1,u2,times,_ = env.compute_signal(x1, x3, T)
+                C_TN = emp_cross_corr(u1, u2, times, verbose=False) 
+                C_TN = C_TN[len(u1)+ index_tau_max]
+                C_TNs[i,j] = C_TN
+    mean_C = np.mean(C_TNs, axis = 0)
+    q_bound = np.quantile(C_TNs, q, axis = 0)
+    Q_bound = np.quantile(C_TNs, 1-q, axis = 0)
+    plt.figure(3)
+    plt.fill_between(Ns, q_bound, Q_bound, alpha=0.15, linewidth=1.5, color='green')
+    plt.plot(Ns, mean_C, linewidth = 1.5, color ='green', marker = 'o')
+    plt.show()
+    
+    #%%
+    L, N, T = 50, 100, 250
+    nb_steps = 50
+    fourier_cov = lambda w : w**2*np.exp(-w**2)
+    env = Environnement(nb_timesteps = 7000, L = L, N = N, 
+                        fourier_cov= fourier_cov)
+    u1,u2,times,_ = env.compute_signal(x1, x1, T)
+    result_autocor = plot_c_asy(x1,x1,env,high_tau=T, low_tau = 10**-4, 
+                                nb_steps= nb_steps)
+    C_TN = emp_cross_corr(u1, u2, times, verbose=False) 
+    plt.figure(4)
+    taus = np.linspace(10**-4,T, nb_steps)
+    plt.plot(taus, result_autocor, label = 'C_asy autocorr', 
+             linewidth = 1.5, color = 'red', marker = '^')
+    plt.plot(times, C_TN[len(u1):], linewidth = 1.5, color = 'blue', 
+             label = 'Empirical cross correlation')
+    plt.legend()
+    plt.show()
+    
+    
+    #%%
+    
     # Evaluation of the error
-    speed = np.linspace(0.1,5,50000)
+    L, N, T = 50, 100, 250
+    nb_steps = 50
+    fourier_cov = lambda w : w**2*np.exp(-w**2)
+    speed = np.linspace(0.1,5,10)
     erreur = []
-    for i in range(len(speed)):
-        erreur.append( ((x2[0]-x1[0])/int((x2[0]-x1[0])/speed[i])-speed[i])/speed[i]*100)
+    for i in trange(len(speed), desc = 'Computing c0'):
+        c = speed[i]
+        env = Environnement(nb_timesteps = 3000, L = L, N = N, 
+                        fourier_cov= fourier_cov, c0 = c)
+        u1,u2,times,_ = env.compute_signal(x1, x3, T)
+        C_TN = emp_cross_corr(u1, u2, times, verbose = False)[len(u1):]
+        argmax_index = np.argmax(C_TN)
+        time_estimator = times[argmax_index]
+        
+        estimator = np.linalg.norm(x1-x3)/time_estimator
+        print('estimator : ', estimator)
+        erreur.append(np.abs(estimator - c))
+    erreur = np.array(erreur)
     # pour c0 > 5 l'erreur est très grande car l'écart mesurable est inférieur à la distance
     plt.plot(speed,erreur)
     plt.title("Error evaluation on the speed estimation with the distance/time ratio")
     plt.xlabel("Speed c0")
-    plt.ylabel("Error %")
+    plt.ylabel("Error")
     plt.show()
-
-#### La suite n'est pas utile non?
-
-#    plot_exp(env,X)
-#    env = Environnement(N = N, L = L, c0 = 1)
-#    #tau = 0
-#    C_TN = emp_cross_corr(tau, x1, x2, T, N, env)
-#    estimated_c0 = 8*np.pi**(3/2)*C_TN
-
-#    with warnings.catch_warnings():
-#        warnings.filterwarnings("ignore",category=RuntimeWarning)
-#        # Question 1
-#        L, N, T = 50, 50, 10**3
-#        x_to_test = []
-#        X = np.zeros((5,2))
-#        X[:,0] = -15 + 5*np.linspace(1,5,5,dtype = 'int')
-#        experience ={}
-#        for j in tqdm(range(len(X))) :
-#            for i in range(j + 1, len(X)) :
-#                taus = np.linspace(1,6, 10)
-#                env = Environnement(N = N, L = L)
-#                x1,x2 = X[j], X[i]
-#                emp_cross_corr1 = exp_emp_cross_cor1 = cross_corr_1 = cross_asy = []
-#                for tau in taus :
-#                    emp_cross_corr1.append(emp_cross_corr(tau, x1, x2, T, N, env, verbose = True))
-#                    exp_emp_cross_cor1.append(exp_emp_cross_cor(tau, x1, x2, N, env, verbose = True))
-#                    cross_corr_1.append(cross_1(tau, x1, x2, L, env, verbose = True))
-#                    cross_asy.append(C_asy(tau, x1, x2,  env, verbose = True))
-#                emp_cross_corr1 = np.array(emp_cross_corr1)
-#                exp_emp_cross_cor1 = np.array(exp_emp_cross_cor1)
-#                cross_corr_1 = np.array(cross_corr_1)
-#                cross_asy = np.array(cross_asy)
-#                temp_dict = {'emp_cross_corr1' : emp_cross_corr1,
-#                             'exp_emp_cross_cor1' : exp_emp_cross_cor1,
-#                             'cross_corr_1' : cross_corr_1,
-#                             'cross_asy' : cross_asy}
-#                experience[(j,i)] = temp_dict
-#        j,i = (0,1)
-#        values = experience[(j,i)]
-#        colors = ['b', 'g', 'r','c','m','y','k']
-#        for key, val in values.items() :
-#            U = randint(low = 0, high = len(colors)).rvs()
-#            plt.plot(val, label= key, color = colors[U])
-#        plt.legend()
-#        plt.show()
-#
-#        # Question 2
-#
-#        Ns,Ts = np.linspace(10,100), np.linspace(100,10**4)
-#        nb_repetitions = 20
-#        c_tns = np.zeros((len(Ns), len(Ts), nb_repetitions))
-#        tau = 1
-#        x1, x2 = X[0], X[1]
-#        env = Environnement(N = N, L = L)
-#        for i in range(len(Ns)) :
-#            for j in range(len(Ts)) :
-#                for k in range(nb_repetitions) :
-#                    c_tns[i,j,k] = emp_cross_corr(tau, x1, x2, Ts[j], Ns[i], env)
-
-#        plt.semilogx(tsav, meanRegret, color = colors[k])
-#        plt.fill_between(tsav, qRegret, QRegret, alpha=0.15, linewidth=1.5, color=colors[k])
-#        plt.xlabel('Time')
-#        plt.ylabel('Regret')``
-
-        # Question 3
+#%%
+    
+                
+                
+    
--- environnement.py
+++ environnement.py
@@ -9,110 +9,89 @@ Created on Thu Feb 21 16:12:47 2019
 import numpy as np
 from scipy.special import j0, y0, hankel1
 from numpy.fft import fft, ifft, fftfreq
-from scipy.integrate import quad
+from tqdm import trange
 
 
-def generate_n_gp(time_points, fourier_cov, return_fourier = True, eps=10**-5):
+def generate_n_gp(time_points,return_fourier = True):
         
-    assert isinstance(time_points, np.ndarray)
-    
+    f = lambda x : np.exp(-x**2/4)*(2-x**2)/(4*np.sqrt(2))
     n = len(time_points)
     y_fourier = fft(np.random.randn(n))
-    delta_t = time_points[1] - time_points[0]
-    freqs = fftfreq(n, d=delta_t)
-    z_fourier = np.sqrt(fourier_cov(freqs)) * y_fourier
+    f_fourier = fft(f(time_points))
+    z_fourier = np.sqrt(f_fourier)*y_fourier
     if return_fourier :
         return z_fourier
     else :
-        return np.real(ifft(z_fourier))
+        inverse_z = ifft(z_fourier)
+        print('Imaginary part :', np.imag(inverse_z))
+        return np.real(inverse_z)
 
 
 class Environnement():
 
-    def __init__(self, c0=1, L=50, N=50, nb_timesteps=100, fourier_cov = lambda w :np.exp(-w**2)) :
+    def __init__(self, c0=1, L=50, N=50, nb_timesteps=100, fourier_cov = lambda 
+                 w : w**2*np.exp(-w**2)) :
 
         self.L = L
         self.N = N
         self.c0 = c0
         self.nb_timesteps = nb_timesteps
-        thetas = np.random.uniform(size=N)
-        self.ys = np.zeros((N, 2))
-        self.ys[:, 0] = self.L * np.cos(2 * np.pi * thetas)
-        self.ys[:, 1] = self.L * np.sin(2 * np.pi * thetas)
+        self.ys = L*self.gen_ys(N)
         self.fourier_cov = fourier_cov
+        
+    def gen_ys(self,N) :
+        thetas = np.random.uniform(low = 0, high = 2*np.pi, size = N)
+        temp_y = np.zeros((N,2))
+        for i in range(N) :
+            temp_y[i] = np.array([np.cos(thetas[i]), np.sin(thetas[i])])
+        return temp_y
 
-    def G_hat(self,w, x, y) : 
+    def G_hat(self, w, x, y) : 
             norme = np.linalg.norm(x - y)/self.c0
             return np.nan_to_num((1j/4*hankel1(0,w*norme)))
         
-    def G(self,t, x, y) : 
+    def G(self, t, x, y) : 
             norm = np.linalg.norm(x - y)/self.c0
             if t > norm: 
-                return 1/(2*np.pi*np.sqrt(t**2 - norm**2))
+                return 1/(2*np.pi*self.c0**2*np.sqrt(t**2 - norm**2))
             else : 
                 return 0
-    def fourier_gaussian(self,w) :
     
-        temp_y = np.random.randn(4)
-        C = np.eye(4)/2
-        C[2,0] = C[0,2]  = 1/2
-        C[3,1] = C[1,3] = -1/2
-        cplx_gaussian = np.dot(C,temp_y)
-        return cplx_gaussian[0]
-    
-
     def compute_signal(self, x1, x2, T):
 
         time_discretization = np.linspace(0, T, self.nb_timesteps)
 
-        tau = (time_discretization[1] -  time_discretization[0])/T
-#        freqs = np.fft.fftfreq(self.nb_timesteps, d=delta_T)
-#        sum_gps_1 = np.zeros(len(freqs))
-#        sum_gps_2 = np.zeros(len(freqs))
-#        for j in range(self.N) : 
-#            y_fourier = fft(np.random.randn(len(freqs)))
-#            z_fourier = np.sqrt(self.fourier_cov(freqs)) * y_fourier
-#            G_freqs_1 = np.array([self.G_hat(w,x1,self.ys[j]) for w in freqs])
-#            G_freqs_2 = np.array([self.G_hat(w,x2,self.ys[j]) for w in freqs])
-#            z_fourier = z_fourier
-#            sum_gps_1 = sum_gps_1 + z_fourier*G_freqs_1
-#            sum_gps_2 = sum_gps_2 + z_fourier*G_freqs_2
-#        
-#        signal_1 = np.real(ifft(sum_gps_1))
-#        signal_2 = np.real(ifft(sum_gps_2))
-        
-        from scipy.signal import fftconvolve
-        
-        random_signal = np.zeros((len(self.ys), len(time_discretization)))
-        signal_11 = np.zeros((len(self.ys), len(time_discretization)))
+        self.tau = T/(self.nb_timesteps - 1)
+        random_signal = np.zeros((len(self.ys), len(time_discretization)), 
+                                 dtype = 'complex')
+        signal_11 = np.zeros((len(self.ys), len(time_discretization)),
+                             dtype = 'complex')
         signal_21 = 1*signal_11
+        freqs = np.fft.fftfreq(self.nb_timesteps, d=self.tau)
+        for k in trange(len(self.ys), desc = 'Computing signals', disable = True) :
+            random_signal[k, :] = generate_n_gp(time_discretization,
+                         return_fourier = True)
+            signal_11[k, :] = np.array([self.G_hat(w,x1,self.ys[k]) 
+            for w in freqs])
+            signal_21[k, :] = np.array([self.G_hat(w,x2,self.ys[k]) 
+            for w in freqs])
         
-        for k in range(len(self.ys)) :
-            random_signal[k, :] = generate_n_gp(time_discretization,self.fourier_cov,return_fourier = False)
-            signal_11[k, :] = np.array([self.G(t,x1,self.ys[k]) for t in time_discretization])
-            signal_21[k, :] = np.array([self.G(t+tau,x2,self.ys[k]) for t in time_discretization])
-        
-        signal_1 = np.zeros((len(self.ys),len(time_discretization)))
-        signal_2 = 1*signal_1
-        for k in range(len(self.ys)) :
-            signal_1[k,:] = fftconvolve(signal_11[k,:],random_signal[k,:], mode = 'same')
-            signal_2[k,:] = fftconvolve(signal_21[k,:],random_signal[k,:], mode = 'same')
-        signal_1 = np.sum(signal_1,axis = 0)
-        signal_2 = np.sum(signal_2,axis = 0)
+        signal_1 = signal_11*random_signal
+        signal_2 = signal_21*random_signal
+        signal_1 = np.real(ifft(np.sum(signal_1,axis = 0)))
+        signal_2 = np.real(ifft(np.sum(signal_2,axis = 0)))
    
-        
-
         return self.c0/ np.sqrt(self.N) * \
             signal_1, self.c0/ np.sqrt(self.N) * \
             signal_2, time_discretization, self.ys
 
 
 if __name__ == '__main__':
-    env = Environnement(N=10, nb_timesteps= 10**3, c0 = 1, L = 50)
-    x1 = x2 = np.ones(2)
-    x2 = 1*x2 
+    env = Environnement(N=30, nb_timesteps= 500, c0 = 1, L = 50)
+    X = np.zeros((5,2))
+    X[:,0] = -15 + 5*np.linspace(1,5,5,dtype = 'int')
+    x1, x2, x3, x4, x5 = X[0], X[1], X[2], X[3], X[4]
     u1, u2, time, _ = env.compute_signal(x1, x2, 100)
-    u1, u2 = u1, u2
     import pylab as plt
     plt.plot(time, u1, color='red', label='u1')
     plt.plot(time, u2, color='blue', label='u2')
